0978243eba5acb65a074fae9980ad6331aa1a3bb
jegerfreddy <vetle.k@hotmail.no>
2025-12-02 22:12:14 +0100

    Added application header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <main>
-
+      <app-header/>
     
       <section>
         <router-outlet/>
