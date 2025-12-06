f4c23a0ce93d6ac4ae9208d2c268b244cb9c9275
Vetle <vetle.k@hotmail.no>
2025-12-06 19:55:47 +0100

    added more buttons to header component and removed ssr config from project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,5 +18,19 @@ import { HeaderComponent } from './components/header/header.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private isDarkModeSubject = new BehaviorSubject<boolean>(false);
+  isDarkMode$ = this.isDarkModeSubject.asObservable();
+
   title = 'vetlegk.github.io';
+
+  constructor() {
+    const currentTheme = localStorage.getItem("vetlegk_page_theme");
+    const isDark = currentTheme === "true"
+    this.setTheme(isDark);
+  }
+
+  setTheme(value: boolean) {
+    this.isDarkModeSubject.next(value);
+    localStorage.setItem("vetlegk_page_theme", value.toString());
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule, House, IdCard, GraduationCap } from 'lucide-angular'
+import { LucideAngularModule, House, IdCard, GraduationCap, MoonIcon, Settings } from 'lucide-angular'
 
 @Component({
   selector: 'app-header',
@@ -12,26 +12,40 @@ import { LucideAngularModule, House, IdCard, GraduationCap } from 'lucide-angula
 
       <div class="nav-buttons-container">
         <button class="nav-button">
-          <lucide-icon [img]="House" size="32" color="white"></lucide-icon>
-          <p style="margin-left: 0.4rem; color: white;">Hjem</p>
+          <lucide-icon [img]="House" size="32" color="#6C4E31"></lucide-icon>
+          <p style="margin-left: 0.4rem; color: #6C4E31;">Hjem</p>
         </button>
 
         <button class="nav-button">
-          <lucide-icon [img]="IdCard" size="32" color="white"></lucide-icon>
-          <p style="margin-left: 0.4rem; color: white;">Om Meg</p>
+          <lucide-icon [img]="IdCard" size="32" color="#6C4E31"></lucide-icon>
+          <p style="margin-left: 0.4rem; color: #6C4E31;">Om Meg</p>
         </button>
 
         <button class="nav-button">
-          <lucide-icon [img]="GraduationCap" size="32" color="white"></lucide-icon>
-          <p style="margin-left: 0.4rem; color: white;">Karakterer</p>
+          <lucide-icon [img]="GraduationCap" size="32" color="#6C4E31"></lucide-icon>
+          <p style="margin-left: 0.4rem; color: #6C4E31;">Karakterer</p>
+        </button>
+
+        <button class="nav-button">
+          <lucide-icon [img]="MoonIcon" size="32" color="#6C4E31"></lucide-icon>
+          <p style="margin-left: 0.4rem; color: #6C4E31;">Dark Mode</p>
+        </button>
+
+        <button class="nav-button">
+          <lucide-icon [img]="Settings" size="32" color="#6C4E31"></lucide-icon>
+          <p style="margin-left: 0.4rem; color: #6C4E31;">Settings</p>
         </button>
       </div>
+
+      
     </section>
   `,
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   House = House;
-  IdCard = IdCard
-  GraduationCap = GraduationCap
+  IdCard = IdCard;
+  GraduationCap = GraduationCap;
+  MoonIcon = MoonIcon;
+  Settings = Settings;
 }
